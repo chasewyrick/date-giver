687413433d07fe4d370d9b79b7b145797dc7ecab
Philippe Loctaux <loctauxphilippe@gmail.com>
2015-11-09 15:17:04 +0100

    added main.c

---
+++ src/main.c
@@ -0,0 +1,18 @@
+/*
+ * birthday-date - main.c
+ *
+ * Philippe Loctaux
+ * phil@pihlippeloctaux.com
+ * License - GPL v2
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+
+int main(int argc, char const *argv[])
+{
+	printf("oh hai\n");
+
+	return 0;
+}
