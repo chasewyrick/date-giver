6c2ecb3ace34298af5830c31b45d15497bb81455
Philippe Loctaux <loctauxphilippe@gmail.com>
2015-11-12 10:44:33 +0100

    added input files

---
+++ src/input.c
@@ -0,0 +1,22 @@
+/*
+ * birthday-date - main.c
+ *
+ * Copyright (c) Philippe Loctaux <phil@pihlippeloctaux.com>
+ * License - GPL v2
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+
+int d = 0;
+
+int input_user()
+{
+	printf("please enter your date of your year\n");
+
+	scanf("%d", &d);
+	printf("you entered %d.\n", d);
+	return 0;
+}
---
+++ src/input.h
@@ -0,0 +1,9 @@
+/*
+ * birthday-date - input.h
+ *
+ * Copyright (c) Philippe Loctaux <phil@pihlippeloctaux.com>
+ * License - GPL v2
+ *
+ */
+
+int input_user();
