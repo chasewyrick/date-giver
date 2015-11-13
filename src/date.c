90b49efb4821da838e22b095accca06f15dba74f
Philippe Loctaux <loctauxphilippe@gmail.com>
2015-11-13 14:47:24 +0100

    added braces otherwise it doesn't work

--- src/date.c
+++ src/date.c
@@ -33,21 +33,21 @@ int process_year()
 	printf("Enter your year, between 1900 and 2099, please enter it with the YYYY format.\n");
 	scanf("%d", &year);
 
-	if (!year < 1900 && !year > 2099)
+	if (!year < 1900 && !year > 2099) {
 		fprintf(stderr, "You've entered an incorrect year.\n");
 		fprintf(stderr, "Please run the program again and try with a valid year.\n");
 		return -1;
-
-
-
-	if (year%400 == 0)
-		printf("You were born in %d, which is a leap year.\n", year);
-	else if (year%100 == 0)
-		printf("You were born in %d, which is not a leap year.\n", year);
-	else if (year%4 == 0)
-		printf("You were born in %d, which is a leap year.\n", year);
-	else
-		printf("You were born in %d, which is not a leap year.\n", year);
+	} else {
+		if (year%400 == 0) {
+			printf("You were born in %d, which is a leap year.\n", year);
+		} else if (year%100 == 0) {
+			printf("You were born in %d, which is not a leap year.\n", year);
+		} else if (year%4 == 0) {
+			printf("You were born in %d, which is a leap year.\n", year);
+		} else {
+			printf("You were born in %d, which is not a leap year.\n", year);
+		}
+	}
 
 	return 0;
 }
