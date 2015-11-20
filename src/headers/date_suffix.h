99313b9555c020d3d1fe9917fbe4a39c22a628fa
Philippe Loctaux <loctauxphilippe@gmail.com>
2015-11-20 16:26:21 +0100

    adding the suffix for the day in it's own file

---
+++ src/headers/date_suffix.h
@@ -0,0 +1,12 @@
+/*
+ * birthday-date - date_suffix.h
+ *
+ * Copyright (c) Philippe Loctaux <phil@philippeloctaux.com>
+ * License - GPL v2
+ *
+ */
+
+int day_1();
+int day_2();
+int day_3();
+int day_other();
