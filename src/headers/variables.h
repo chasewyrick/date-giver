616747bf0320b455a5d64b42df5c4e0cd6470197
Philippe Loctaux <loctauxphilippe@gmail.com>
2015-11-20 16:12:55 +0100

    added variables

---
+++ src/headers/variables.h
@@ -0,0 +1,16 @@
+/*
+ * birthday-date - variables.h
+ *
+ * Copyright (c) Philippe Loctaux <phil@pihlippeloctaux.com>
+ * License - GPL v2
+ *
+ */
+
+extern int year;
+extern int month;
+extern int day;
+
+extern char day_suffix_1[];
+extern char day_suffix_2[];
+extern char day_suffix_3[];
+extern char day_suffix[];
