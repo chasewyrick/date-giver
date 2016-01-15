d7069282f6b60baba72db00eb960239dc4f935dd
Philippe Loctaux <phil@philippeloctaux.com>
2016-01-15 10:28:10 +0100

    the error still doesn't runs

--- src/date.c
+++ src/date.c
@@ -22,29 +22,21 @@ int process_year()
 
 	if (year < 1900 && year > 2099) {
 		fprintf(stderr, "Error.\nPlease run the program again and try with a valid year.\n");
-		return 1;
+		exit(EXIT_FAILURE);
 	}
-
-	return 0;
 }
 
 int process_month()
 {
 	input_month();
-
-	return 0;
 }
 
 int process_day()
 {
 	input_day();
-
-	return 0;
 }
 
 int display_date()
 {
 	printf("Date given: %d/%d/%d\n", year, month, day);
-
-	return 0;
 }
