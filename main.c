f58e349cce599b2a6d1a0576378cbec389df8224
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-01-07 21:57:09 +0100

    main.c: comments and libraries

--- main.c
+++ main.c
@@ -1,15 +1,12 @@
-/*
- * date-giver - main.c
- *
- * Copyright (c) Philippe Loctaux <phil@pihlippeloctaux.com>
- * License - GPL v2
- *
+/**
+ * File              : main.c
+ * Author            : Philippe Loctaux <loctauxphilippe@gmail.com>
+ * Date              : 09.11.2015
+ * Last Modified Date: 07.01.2018
+ * Last Modified By  : Philippe Loctaux <loctauxphilippe@gmail.com>
  */
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <ncurses.h>
-
 #include "variables.h"
 #include "date.h"
 
