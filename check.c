89ca0ec7f7df5acd53e183b000864b9752f975de
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-01-08 17:09:59 +0100

    check.c: added check for the year

--- check.c
+++ check.c
@@ -7,20 +7,26 @@
  */
 
 #include <stdio.h>
+/* use of the stdblib library to provide the exit function */
+#include <stdlib.h>
 #include "variables.h"
 #include "check.h"
 
-int check_year(int)
+int check_year(void)
 {
-	;
+	if (year < 1900 && year > 2099) {
+		fprintf(stderr, "invalid year.\n");
+		fprintf(stderr, "please try again with a valid year.\n");
+		exit(EXIT_FAILURE);
+	}
 }
 
-int check_month(int)
+int check_month(void)
 {
-	;
+	//
 }
 
-int check_day(int)
+int check_day(void)
 {
-	;
+	//
 }
